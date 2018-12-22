283e26724a29b1a61cf0d2ae6b4ae30f1c69e15f
kalais263 <42733833+kalais263@users.noreply.github.com>
2018-12-22 11:51:38 +0530

    Update Odd.java

--- Odd.java
+++ Odd.java
@@ -10,12 +10,18 @@ System.out.println("Enter the first value");
 a=sc.nextInt();
 System.out.println("Enter the second value");
 b=sc.nextInt();
+if((a<=100000)&&(b<=100000))
+{
 for(i=a;i<=b-1;i++)
 {
 if((i+1)%2==1)
 {
 System.out.println(i+1);
 }
+}}
+  else
+  {
+    System.out.println("Check the number");
 }
 }
 }
