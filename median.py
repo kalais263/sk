2e4df60e9b8bc1fc53095e17ce6e8d3b9dfb8d92
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-01-25 21:46:04 +0530

    Create median.py

---
+++ median.py
@@ -0,0 +1,9 @@
+number=int(input("Enter the length:"))
+list=[]
+for i in range(0,number)
+  newNo=int(input(''))
+  a=list.append(newNo)
+  print list
+result=list.sort()
+i=len(list)/2
+print("Median element is:",list[i])
