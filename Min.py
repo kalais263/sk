ca15cd3d7442150dd414bb5753ba65095e39d246
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-01-25 21:33:44 +0530

    Create Min.py

---
+++ Min.py
@@ -0,0 +1,8 @@
+number=int(input("Enter the length:"))
+list=[]
+for i in range(0,number):
+  newNo=int(input(''))
+  a=list.append(newNo)
+  print list
+result=min(list)
+print ("Minimum element is",result)
