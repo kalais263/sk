664ad2ae5bb27547f799b6d6e192db0dd460b1f8
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-03-08 09:49:47 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "person";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn -> connect_error) 
+{
+die("connection failed");
+}
+if($_SERVER["REQUEST_METHOD"] =="POST")
+{
+$raw = json_decode(file_get_contents('php://input'),true);
+$Username= $raw['username'];
+$Password= $raw['password'];
+$result = $conn->query("SELECT * FROM register where username='".$Username."'");
+$data = array();
+if($result->num_rows > 0)
+{
+$row = $result->fetch_assoc();
+$Password_from_db = $row["password"];
+if($Password == $Password_from_db)
+{
+$token='".$token."' WHERE username='".$Username."'";
+$conn->query($sql);
+$response = array('status' => 'success', 'msg' => 'Logged in successfully', session_id_key' => $token);
+echo json_encode($response);
+}
+else
+{
+$response= array('status' => 'Password is not matching');
+echo json_encode($response);
+}
+}
+else
+{
+$response = array('status' => 'Email id is not match');
+echo json_encode($response);
+}
+$conn ->close();
+}
+
