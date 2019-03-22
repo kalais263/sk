80f28745869ca52a82ea067d8247907f1c7bde0f
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-03-22 14:12:40 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner
+class Sample
+{
+public static void main(String args[])
+{
+int num1,num2;
+Scanner sc=new Scanner(System.in);
+num1=sc.nextInt();
+num2=sc.nextInt();
+num1=num1^num2;
+num2=num1^num2;
+num1=num1^num2;
+sc.close();
+System.out.println(num1);
+System.out.println(num2);
+}
+}
